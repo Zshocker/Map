257e21962165664a9964f60473e0dd1226630ce7
Zshocker <hicham.sentel-etu@etu.univh2c.ma>
2021-11-05 10:25:16 +0100

    fd

--- Map/Hotel.cpp
+++ Map/Hotel.cpp
@@ -1,1 +1,29 @@
 #include "Hotel.h"
+
+GeoMap::Hotel::Hotel(float lat, float lng, int code, string S, string nom, int nbreCh):SemantiquePoint(lat,lng,code,S)
+{
+	this->nmbrChambre = nbreCh;
+	this->nom = nom;
+}
+
+bool GeoMap::Hotel::VerfierDispo() const
+{
+	return false;
+}
+
+GeoMap::Hotel::Hotel(const Hotel&H):SemantiquePoint(H)
+{
+	this->nom = H.nom;
+	this->nmbrChambre = H.nmbrChambre;
+}
+
+Hotel& GeoMap::Hotel::operator=(const Hotel& C)
+{
+	if (this != &C) 
+	{
+		this->SemantiquePoint::operator=(C);
+		this->nom = C.nom;
+		this->nmbrChambre = C.nmbrChambre;
+	}
+	return *this;
+}
--- Map/Hotel.h
+++ Map/Hotel.h
@@ -1,13 +1,17 @@
 #pragma once
 #include "SemantiquePoint.h"
-namespace GeoPoint {
+namespace GeoMap {
 	class Hotel :
 		private SemantiquePoint
 	{
-
-
-
-
+	private:
+		string nom;
+		int nmbrChambre;
+	public:
+		Hotel(float lat = 0, float lng = 0, int code = 0, string S = "", string nom = "", int nbreCh=0);
+		bool VerfierDispo()const ;
+		Hotel(const Hotel&);
+		Hotel& operator=(const Hotel&);
 	};
 }
 
